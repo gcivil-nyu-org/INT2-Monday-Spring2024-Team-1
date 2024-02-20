477799c8a063de319264f4181193b51b07039f40
Shreyas K N <shreyas2499@gmail.com>
2024-02-20 15:59:47 -0500

    Updated folder names

--- healthScore/healthScore/apps.py
+++ healthScore/healthScore/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HealthConfig(AppConfig):
+class HealthscoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'health'
+    name = 'healthScore'
