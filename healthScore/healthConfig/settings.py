56f2dfb8f143064a4aee9aaa78c0c1007d54e7cb
Sarthak Goel <sarthakgoel@Sarthaks-MBP.lan>
2024-02-23 02:46:42 -0500

    test aws sql connection

--- healthScore/healthConfig/settings.py
+++ healthScore/healthConfig/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-# import pymysql
+import pymysql
 
-# pymysql.install_as_MySQLdb()
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'healthConfig.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'HealthScore',
-#         'USER': 'root',
-#         'PASSWORD': '', # change it with your sql server password for local testing
-#         'HOST':'localhost',
-#         'PORT':'3306',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'HealthScore',
+        'USER': 'admin',
+        'PASSWORD': 'healthscoreadmin', # change it with your sql server password for local testing
+        'HOST':'awseb-e-meky4ftcfy-stack-awsebrdsdatabase-yqgueanvxaev.clkwykuegjia.us-west-2.rds.amazonaws.com',
+        'PORT':'3306',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
