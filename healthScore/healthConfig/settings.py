5560504ad55f003fa562d3314b36e9becc408ab7
Sarthak Goel <sarthakgoel@Sarthaks-MBP.lan>
2024-02-23 02:07:50 -0500

    comment mysql

--- healthScore/healthConfig/settings.py
+++ healthScore/healthConfig/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-import pymysql
+# import pymysql
 
-pymysql.install_as_MySQLdb()
+# pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
