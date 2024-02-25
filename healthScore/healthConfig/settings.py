0a4dd32e4acc066213b46d441c548f3641b5212c
Sarthak Goel <sarthakgoel@Sarthaks-MBP.lan>
2024-02-25 18:22:45 -0500

    add db changes

--- healthScore/healthConfig/settings.py
+++ healthScore/healthConfig/settings.py
@@ -81,9 +81,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'HealthScore',
-        'USER': 'root',
-        'PASSWORD': '', # change it with your sql server password for local testing
-        'HOST':'localhost',
+        'USER': 'admin',
+        'PASSWORD': 'HealthScoreAdmin', # change it with your sql server password for local testing
+        'HOST':'http://awseb-e-meky4ftcfy-stack-awsebrdsdatabase-yqgueanvxaev.clkwykuegjia.us-west-2.rds.amazonaws.com/',
         'PORT':'3306',
     }
 }
