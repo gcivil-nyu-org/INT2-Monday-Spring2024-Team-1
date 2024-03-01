a7228778a6bad7d0f4de29a63dfbb2d10c0f107f
Sam Geier <sgeier19@gmail.com>
2024-03-01 16:42:57 -0500

    black formatting fix

--- healthScore/healthScore/views.py
+++ healthScore/healthScore/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.utils import timezone
 from datetime import datetime, timedelta
+
 # from django.contrib import messages
 from django.contrib.auth.hashers import make_password
 import json
