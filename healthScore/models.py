92bca2f3d4c7fd0ad7c0696074c2ca8184e7de68
jazwu <jaz.wu@outlook.com>
2024-03-08 08:49:43 -0500

    user model change

--- healthScore/models.py
+++ healthScore/models.py
@@ -72,7 +72,7 @@ class User(AbstractBaseUser, PermissionsMixin):  # Viewed by User
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
-    date_joined = models.DateTimeField(default=timezone.now())
+    # date_joined = models.DateTimeField(default=timezone.now())
     last_login = models.DateTimeField(blank=True, null=True)
 
     username = models.CharField(max_length=50, unique=True)
@@ -97,6 +97,10 @@ class User(AbstractBaseUser, PermissionsMixin):  # Viewed by User
     REQUIRED_FIELDS = ["username"]
 
     objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
 
     def get_full_name(self):
         return self.name
