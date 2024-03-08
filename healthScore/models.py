e52c2bc2436761cb3b7923f5e620df691a5d97b1
jazwu <jaz.wu@outlook.com>
2024-03-08 08:12:19 -0500

    user model change

--- healthScore/models.py
+++ healthScore/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
     AbstractBaseUser,
 )
+from django.utils import timezone
 
 
 STATUS_CHOICES = [
@@ -14,7 +15,7 @@ STATUS_CHOICES = [
 
 
 # Create your models here.
-class hospital(models.Model):  # Viewed by healthScoreAdmin and hospitalAdmin
+class Hospital(models.Model):  # Viewed by healthScoreAdmin and hospitalAdmin
     id = models.AutoField(primary_key=True)
     name = models.TextField(null=False)
     address = models.TextField(null=False)
@@ -25,7 +26,7 @@ class hospital(models.Model):  # Viewed by healthScoreAdmin and hospitalAdmin
     status = models.TextField(choices=STATUS_CHOICES, default="pending")
 
 
-class hospitalStaff(models.Model):  # Viewed by hospitalAdmin
+class HospitalStaff(models.Model):  # Viewed by hospitalAdmin
     id = models.AutoField(primary_key=True)
     hospitalID = models.ForeignKey("hospital", to_field="id", on_delete=models.CASCADE)
     admin = models.BooleanField(default=False)  # True = hospitalAdmin, False = Doctor
@@ -43,60 +44,68 @@ class hospitalStaff(models.Model):  # Viewed by hospitalAdmin
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError("Users must have an email address")
-        user = self.model(
-            email=self.normalize_email(email), username=username, **extra_fields
-        )
+            raise ValueError("You have not provided a valid e-mail address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
+    
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, username, password=None, **extra_fields):
-        extra_fields.setdefault("is_superuser", True)
+    def create_superuser(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
 
-        return self.create_user(email, username, password, **extra_fields)
 
-
-class user(AbstractBaseUser, PermissionsMixin):  # Viewed by User
+class User(AbstractBaseUser, PermissionsMixin):  # Viewed by User
     id = models.AutoField(primary_key=True)
-    email = models.EmailField(null=False)
-    name = models.TextField(null=False)
+    email = models.EmailField(unique=True)
     password = models.TextField(null=False)
-    username = models.CharField(null=False, max_length=50, unique=True)
-    dob = models.DateField(null=False)
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now())
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    username = models.CharField(max_length=50, unique=True)
+    name = models.TextField(blank=True, max_length=255, default='')
+    dob = models.DateField()
     contactInfo = models.TextField(default="", max_length=10)
-    proofOfIdentity = models.TextField(
-        null=False
-    )  # Convert image to base64 string and store it here
     address = models.TextField(null=False)
-    securityQues = models.TextField(
-        default=""
-    )  # If we not doing email resetting password
-    securityAns = models.TextField(
-        default=""
-    )  # If we not doing email resetting password
-    gender = models.TextField(
-        default=""
-    )  # Can be updated to a choice field later on, if needed
-    profilePic = models.TextField(
-        null=True
-    )  # Convert image to base64 string and store it here
+    proofOfIdentity = models.TextField(null=False)  # Convert image to base64 string and store it here
+    
+    securityQues = models.TextField(default="")  # If we not doing email resetting password
+    securityAns = models.TextField(default="")  # If we not doing email resetting password
+    gender = models.TextField(blank=True, default="")  # Can be updated to a choice field later on, if needed
+    profilePic = models.TextField(null=True)  # Convert image to base64 string and store it here
     bloodGroup = models.TextField(null=False)
     requests = models.JSONField(
         null=True
     )  # Will store data in this form: [{ requestedBy: '', dateTime:'', status:''}]. Will have
     # the data of all the requests that have been done to view the user's records
 
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["email"]
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
     objects = CustomUserManager()
 
+    def get_full_name(self):
+        return self.name
+    
+    def get_short_name(self):
+        return self.username
 
-class healthRecord(models.Model):  # Viewed by User and hospitalStaff who are doctors
+
+class HealthRecord(models.Model):  # Viewed by User and hospitalStaff who are doctors
     id = models.AutoField(primary_key=True)
     doctorID = models.IntegerField(null=False)
     userID = models.ForeignKey("user", to_field="id", on_delete=models.CASCADE)
@@ -110,7 +119,7 @@ class healthRecord(models.Model):  # Viewed by User and hospitalStaff who are do
     healthDocuments = models.JSONField(null=True)
 
 
-class appointment(models.Model):  # Viewed by User and hospitalStaff who are doctors
+class Appointment(models.Model):  # Viewed by User and hospitalStaff who are doctors
     id = models.AutoField(primary_key=True)
     name = models.TextField(null=False)
     properties = models.JSONField(null=True)
@@ -121,7 +130,7 @@ class appointment(models.Model):  # Viewed by User and hospitalStaff who are doc
     # }
 
 
-class communityInteraction(models.Model):
+class CommunityInteraction(models.Model):
     userID = models.ForeignKey("user", to_field="id", on_delete=models.CASCADE)
     postID = models.AutoField(primary_key=True)
     postTitle = models.TextField(null=False)
