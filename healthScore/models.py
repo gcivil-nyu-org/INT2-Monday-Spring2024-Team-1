79b5ef107f0f7e7902b6807794479a52bad17dc6
jazwu <jaz.wu@outlook.com>
2024-03-08 08:51:04 -0500

    add gitignore

--- healthScore/models.py
+++ healthScore/models.py
@@ -72,7 +72,7 @@ class User(AbstractBaseUser, PermissionsMixin):  # Viewed by User
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
-    # date_joined = models.DateTimeField(default=timezone.now())
+    date_joined = models.DateTimeField(default=timezone.now())
     last_login = models.DateTimeField(blank=True, null=True)
 
     username = models.CharField(max_length=50, unique=True)
