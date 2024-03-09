22f0c5976dec88c84699863b856b038675111e05
jazwu <jaz.wu@outlook.com>
2024-03-08 22:12:32 -0500

    reset password feature

--- healthScore/urls.py
+++ healthScore/urls.py
@@ -15,12 +15,20 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("viewRequests", views.view_health_history_requests, name="view_requests"),
 
-    path("password-reset", 
-         auth_views.PasswordResetView.as_view(template_name="password_reset.html"), 
-         name="password_reset"),
-    path("password-reset-sent", 
-         auth_views.PasswordResetDoneView.as_view(),
-         name="password_reset_sent"),
-    path("password-change", auth_views.PasswordChangeView.as_view()),
-    path("password-change-done", auth_views.PasswordChangeDoneView.as_view())
+    path("password-reset/", 
+         auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+         name="password_reset"
+         ),
+    path("password-reset-done/", 
+         auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"),
+         name="password_reset_done"
+         ),
+    path("reset/<uidb64>/<token>/", 
+         auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),
+         name="password_reset_confirm"
+         ),
+    path("password-reset-complete",
+         auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
+         name="password_reset_complete"
+         )
 ]
