a11a3dc4f4843738229e9a2c9058c6a01859e99e
Sam Geier <sgeier19@gmail.com>
2024-04-03 20:14:46 -0400

    flake8

--- healthScore/tests.py
+++ healthScore/tests.py
@@ -1,4 +1,4 @@
-from django.test import RequestFactory, TransactionTestCase, TestCase, Client
+from django.test import RequestFactory, TransactionTestCase, TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.http import HttpRequest
