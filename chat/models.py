a93de42fef28f5324ac409c2bfd7f3c95b50c76d
jazwu <jaz.wu@outlook.com>
2024-04-11 15:10:47 -0400

    start a chat app

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from healthScore.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
