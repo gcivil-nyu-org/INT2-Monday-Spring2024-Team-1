ee38fe942ed5c10e3e60b2b0caf6766599cdc191
Sarthak Goel <sarthakgoel_97@hotmail.com>
2024-04-14 20:54:55 -0400

    Revert "testing fix"
    This reverts commit 89ce33cb9b5af6c4b6b326e37898e446ee18d6a3.

--- healthConfig/asgi.py
+++ healthConfig/asgi.py
@@ -10,9 +10,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
-django_asgi_app = get_asgi_application()
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -20,6 +17,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from chat import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "healthConfig.settings")
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
