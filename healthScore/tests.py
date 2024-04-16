1029c38853fc4922cded7450b2c50f7f8fb5da71
Shreyas K N <shreyas2499@gmail.com>
2024-04-15 20:51:15 -0400

    Updated black .

--- healthScore/tests.py
+++ healthScore/tests.py
@@ -25,8 +25,6 @@ from healthScore.models import (
 from healthScore.file_upload import file_upload
 
 
-from healthScore.homepage_and_auth import login_view, registration, homepage
-
 from healthScore.profile_view import edit_user_info, view_user_info
 
 from healthScore.patient_view_records import (
@@ -45,18 +43,13 @@ from healthScore.patient_submit_health_record import (
 )
 
 from healthScore.healthcare_data import (
-    get_facility_doctors,
-    get_facility_admins,
-    hospital_staff_directory,
     add_healthcare_staff,
     deactivate_healthcare_staff,
     activate_healthcare_staff,
 )
 
 from healthScore.community_data import (
-    community_home,
     view_all_posts,
-    view_my_posts,
     view_post,
     create_post,
     edit_post,
@@ -67,26 +60,15 @@ from healthScore.community_data import (
 
 from healthScore.external_health_request_access import (
     request_health_history,
-    send_approval_emails,
-    send_rejection_emails,
-    update_request_status,
     view_health_history_access_requests,
 )
 
 from healthScore.admin_view_user_healthrecords import (
-    view_healthworkers_user_record,
     admin_view_health_history_requests,
     get_admin_edit,
 )
 
-from healthScore.doctor_data import (
-    get_patients,
-    get_doctor_details,
-    get_patient_details,
-)
 
-
-from healthScore.healthscore_admin_view import update_hospital_status, list_hospitals
 
 DATE_FORMAT = "%Y-%m-%d"
 
