e8f72d436a0acd6406309f4a713341435fbc6792
Shreyas K N <shreyas2499@gmail.com>
2024-04-19 15:32:13 -0400

    Reformatting

--- manage.py
+++ manage.py
@@ -2,9 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.core.management.commands.runserver import Command as runserver
-from django.conf import settings
-
 
 def main():
     """Run administrative tasks."""
