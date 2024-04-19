f78dd768dc2ec8d5553a96ececb83e6124282456
Shreyas K N <shreyas2499@gmail.com>
2024-04-19 01:53:47 -0400

    Updated routing.py

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,22 @@
-from django.urls import re_path
+# from django.urls import re_path
 from . import consumers
 
-websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<receiver_id>\d+)/$", consumers.ChatConsumer.as_asgi()),
-]
+# websocket_urlpatterns = [
+#     re_path(r"ws/chat/(?P<receiver_id>\d+)/$", consumers.ChatConsumer.as_asgi()),
+# ]
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path, re_path
+
+
+application = ProtocolTypeRouter({
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+                    re_path(r"ws/chat/(?P<receiver_id>\d+)/$", consumers.ChatConsumer.as_asgi()),
+            ])
+        )
+    ),
+})
